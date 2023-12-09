5aa2cbea4e1c40388e5ac7f84a0a8ca18b3956ae
Long Vu <long.vudai01@gmail.com>
2023-12-09 17:36:58 +0700

    downgraph version

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "CommonKitUI",
-    platforms: [.iOS(.v15), .macOS(.v12), .macCatalyst(.v15)],
+    platforms: [.iOS(.v14), .macOS(.v11), .macCatalyst(.v14)],
     products: [
         .library(
             name: "ViewComponent",
