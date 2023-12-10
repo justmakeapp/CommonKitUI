65661a21d6d2e94db4433b6f9ffda418f5949a49
Long Vu <long.vudai01@gmail.com>
2023-12-10 15:44:04 +0700

    add CustomEmptyView

---
+++ Sources/ViewComponent/CustomEmptyView.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//
+//
+//  Created by Long Vu on 10/12/2023.
+//
+
+import SwiftUI
+
+public struct CustomEmptyView: View {
+    private let title: String
+
+    public init(title: String) {
+        self.title = title
+    }
+
+    public var body: some View {
+        VStack {
+            Image(systemName: "tray")
+                .font(.largeTitle)
+                .imageScale(.large)
+
+            Text(title)
+                .font(.title3.weight(.semibold))
+        }
+        .foregroundColor(.secondary)
+        .padding()
+    }
+}
