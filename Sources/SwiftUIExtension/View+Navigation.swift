b50dcd5aa207549d0bacd5ef3a42088694098947
Long Vu <long.vudai01@gmail.com>
2024-01-20 13:22:35 +0700

    add navigationDestination

---
+++ Sources/SwiftUIExtension/View+Navigation.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//
+//
+//  Created by Long Vu on 20/01/2024.
+//
+
+import SwiftUI
+
+public extension View {
+    @available(iOS 16.0, *)
+    func navigationDestination<Value>(
+        using value: Binding<Value?>,
+        content: (Value) -> some View
+    ) -> some View {
+        let binding = Binding<Bool>(
+            get: { value.wrappedValue != nil },
+            set: { _ in value.wrappedValue = nil }
+        )
+        return navigationDestination(isPresented: binding) {
+            content(value.wrappedValue!)
+        }
+    }
+}
