5994b305042958874177dc365ee3819c9357bfec
Long Vu <long.vudai01@gmail.com>
2024-01-26 23:14:46 +0700

    add FeedbackManager

---
+++ Sources/ViewComponent/FeedbackManager.swift
@@ -0,0 +1,25 @@
+//
+//  FeedbackManager.swift
+//
+//
+//  Created by Long Vu on 15/07/2023.
+//
+
+import Foundation
+#if os(iOS)
+    import UIKit
+#endif
+#if os(macOS)
+    import AppKit
+#endif
+
+public enum FeedbackManager {
+    public static func selectionChangedFeedback() {
+        #if os(iOS)
+            UISelectionFeedbackGenerator().selectionChanged()
+        #endif
+        #if os(macOS)
+            NSHapticFeedbackManager.defaultPerformer.perform(.levelChange, performanceTime: .now)
+        #endif
+    }
+}
