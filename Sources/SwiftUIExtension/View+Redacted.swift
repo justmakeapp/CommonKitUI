fc9eed016196aa9865bece83ca46b94859d8bbb0
Long Vu <long.vudai01@gmail.com>
2024-02-02 22:53:54 +0700

    add redacted(if

---
+++ Sources/SwiftUIExtension/View+Redacted.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//
+//
+//  Created by Long Vu on 02/02/2024.
+//
+
+import Foundation
+import SwiftUI
+
+public extension View {
+    @ViewBuilder
+    func redacted(if condition: @autoclosure () -> Bool) -> some View {
+        redacted(reason: condition() ? .placeholder : [])
+    }
+}
