0904596e6c97a1a07ea7091fa3911299a7198f6a
Long Vu <long.vudai01@gmail.com>
2024-02-05 20:00:25 +0700

    add TypewriterView

---
+++ Sources/ViewComponent/TypewriterView.swift
@@ -0,0 +1,58 @@
+//
+//  TypewriterView.swift
+//
+//
+//  Created by Long Vu on 05/02/2024.
+//
+
+import SwiftUI
+
+@available(iOS 16.0, *)
+public struct TypewriterView: View {
+    private var text: String
+    private var typingDelay: Duration
+
+    @State private var animatedText: AttributedString = ""
+    @State private var typingTask: Task<Void, Error>?
+
+    public init(
+        text: String,
+        typingDelay: Duration = .milliseconds(30)
+    ) {
+        self.text = text
+        self.typingDelay = typingDelay
+    }
+
+    public var body: some View {
+        Text(animatedText)
+            .onChange(of: text) { _ in animateText() }
+            .onAppear { animateText() }
+    }
+
+    private func animateText() {
+        typingTask?.cancel()
+
+        typingTask = Task {
+            let defaultAttributes = AttributeContainer()
+            animatedText = AttributedString(
+                text,
+                attributes: defaultAttributes.foregroundColor(.clear)
+            )
+
+            var index = animatedText.startIndex
+            while index < animatedText.endIndex {
+                try Task.checkCancellation()
+
+                // Update the style
+                animatedText[animatedText.startIndex ... index]
+                    .setAttributes(defaultAttributes)
+
+                // Wait
+                try await Task.sleep(for: typingDelay)
+
+                // Advance the index, character by character
+                index = animatedText.index(afterCharacter: index)
+            }
+        }
+    }
+}
