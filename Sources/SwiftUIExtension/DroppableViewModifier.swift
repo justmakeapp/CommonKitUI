b816f62b98c344b9bb6ef8b6f5bc072460d0da44
Long Vu <long.vudai01@gmail.com>
2024-03-30 22:54:52 +0700

    add TransferableDroppableViewModifier

--- Sources/SwiftUIExtension/DroppableViewModifier.swift
+++ Sources/SwiftUIExtension/DroppableViewModifier.swift
@@ -35,3 +35,50 @@ public struct DroppableViewModifier: ViewModifier {
         }
     }
 }
+
+@available(iOS 16.0, macOS 13.0, *)
+public struct TransferableDroppableViewModifier<T: Transferable>: ViewModifier {
+    @State private var isTargeted = false
+
+    let payloadType: T.Type
+    let action: (_ items: [T], _ location: CGPoint) -> Bool
+
+    public init(
+        for payloadType: T.Type,
+        action: @escaping (_ items: [T], _ location: CGPoint) -> Bool
+    ) {
+        self.payloadType = payloadType
+        self.action = action
+    }
+
+    public func body(content: Content) -> some View {
+        content
+            .dropDestination(for: payloadType, action: action, isTargeted: {
+                self.isTargeted = $0
+            })
+            .overlay {
+                if isTargeted {
+                    ZStack {
+                        Color.black.opacity(0.5)
+
+                        DroppableView()
+                    }
+                }
+            }
+            .animation(.default, value: isTargeted)
+    }
+}
+
+private struct DroppableView: View {
+    var body: some View {
+        VStack(spacing: 8) {
+            Image(systemName: "plus.circle.fill")
+                .font(.largeTitle)
+            Text("Drop your item here...")
+        }
+        .font(.largeTitle.weight(.bold))
+        .foregroundColor(.white)
+        .frame(maxWidth: 250)
+        .multilineTextAlignment(.center)
+    }
+}
