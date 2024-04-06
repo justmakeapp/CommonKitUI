b7a6725bd945d1bffe7b29f4b12aac34e637003e
Long Vu <long.vudai01@gmail.com>
2024-04-06 16:49:17 +0700

    add disabled

--- Sources/SwiftUIExtension/DroppableViewModifier.swift
+++ Sources/SwiftUIExtension/DroppableViewModifier.swift
@@ -41,31 +41,38 @@ public struct TransferableDroppableViewModifier<T: Transferable>: ViewModifier {
     @State private var isTargeted = false
 
     let payloadType: T.Type
+    let disabled: Bool
     let action: (_ items: [T], _ location: CGPoint) -> Bool
 
     public init(
         for payloadType: T.Type,
+        disabled: Bool = false,
         action: @escaping (_ items: [T], _ location: CGPoint) -> Bool
     ) {
         self.payloadType = payloadType
+        self.disabled = disabled
         self.action = action
     }
 
     public func body(content: Content) -> some View {
-        content
-            .dropDestination(for: payloadType, action: action, isTargeted: {
-                self.isTargeted = $0
-            })
-            .overlay {
-                if isTargeted {
-                    ZStack {
-                        Color.black.opacity(0.5)
+        if disabled {
+            content
+        } else {
+            content
+                .dropDestination(for: payloadType, action: action, isTargeted: {
+                    self.isTargeted = $0
+                })
+                .overlay {
+                    if isTargeted {
+                        ZStack {
+                            Color.black.opacity(0.5)
 
-                        DroppableView()
+                            DroppableView()
+                        }
                     }
                 }
-            }
-            .animation(.default, value: isTargeted)
+                .animation(.default, value: isTargeted)
+        }
     }
 }
 
