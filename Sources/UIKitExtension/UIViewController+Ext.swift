c2f274694119f940f0b5203ef3603f78443274a4
Long Vu <long.vudai01@gmail.com>
2025-01-14 17:20:02 +0700

    Refactor UIViewController extension to ensure compatibility with conditional UIKit imports

--- Sources/UIKitExtension/UIViewController+Ext.swift
+++ Sources/UIKitExtension/UIViewController+Ext.swift
@@ -5,22 +5,24 @@
 //  Created by Long Vu on 13/1/25.
 //
 
-import UIKit
+#if canImport(UIKit)
+    import UIKit
 
-public extension UIViewController {
-    var topmostPresentedViewController: UIViewController? {
-        presentedViewController?.topmostPresentedViewController ?? self
-    }
+    public extension UIViewController {
+        var topmostPresentedViewController: UIViewController? {
+            presentedViewController?.topmostPresentedViewController ?? self
+        }
 
-    var topmostViewController: UIViewController? {
-        if let controller = (self as? UINavigationController)?.visibleViewController {
-            return controller.topmostViewController
-        } else if let controller = (self as? UITabBarController)?.selectedViewController {
-            return controller.topmostViewController
-        } else if let controller = presentedViewController {
-            return controller.topmostViewController
-        } else {
-            return self
+        var topmostViewController: UIViewController? {
+            if let controller = (self as? UINavigationController)?.visibleViewController {
+                return controller.topmostViewController
+            } else if let controller = (self as? UITabBarController)?.selectedViewController {
+                return controller.topmostViewController
+            } else if let controller = presentedViewController {
+                return controller.topmostViewController
+            } else {
+                return self
+            }
         }
     }
-}
+#endif
