eac115cdfdf0cd1c4163e3d6803f760f03050105
Long Vu <long.vudai01@gmail.com>
2025-03-05 17:47:05 +0700

    Add PdfView component for displaying PDF documents in SwiftUI

---
+++ Sources/ViewComponent/PdfView/PdfView.swift
@@ -0,0 +1,56 @@
+//
+//  PdfView.swift
+//  AppTopicKit
+//
+//  Created by Long Vu on 5/3/25.
+//
+
+import SwiftUI
+#if os(iOS)
+    import UIKit
+#endif
+#if os(macOS)
+    import AppKit
+#endif
+
+import PDFKit
+
+public struct PdfKitView {
+    let url: URL
+
+    public init(url: URL) {
+        self.url = url
+    }
+}
+
+#if os(iOS)
+    extension PdfKitView: UIViewRepresentable {
+        public func makeUIView(context _: UIViewRepresentableContext<PdfKitView>) -> PDFView {
+            // Creating a new PDFVIew and adding a document to it
+            let pdfView = PDFView()
+            pdfView.document = PDFDocument(url: self.url)
+            pdfView.autoScales = true
+            return pdfView
+        }
+
+        public func updateUIView(_: PDFView, context _: UIViewRepresentableContext<PdfKitView>) {
+            // we will leave this empty as we don't need to update the PDF
+        }
+    }
+#endif
+
+#if os(macOS)
+    extension PdfKitView: NSViewRepresentable {
+        public func makeNSView(context _: Context) -> PDFView {
+            let pdfView = PDFView()
+            pdfView.document = PDFDocument(url: self.url)
+            return pdfView
+        }
+
+        public func updateNSView(_: PDFView, context _: Context) {}
+    }
+#endif
+
+// #Preview {
+//    PdfView(url: .init(string: "https://pdfobject.com/pdf/sample.pdf")!)
+// }
