a9705f9e09fef6fdadca7450fbcc79b29fc4aa10
Long Vu <long.vudai01@gmail.com>
2025-09-04 16:05:46 +0700

    Add ShakeEffect geometry effect for shake animation in SwiftUI

---
+++ Sources/ViewComponent/ShakeEffect.swift
@@ -0,0 +1,25 @@
+// Credit: https://www.objc.io/blog/2019/10/01/swiftui-shake-animation/
+
+import SwiftUI
+
+public struct ShakeEffect: GeometryEffect {
+    public var amount: CGFloat
+    public var shakesPerUnit: Int
+    public var animatableData: CGFloat
+
+    public init(
+        amount: CGFloat = 10,
+        shakesPerUnit: Int = 3,
+        animatableData: CGFloat
+    ) {
+        self.amount = amount
+        self.shakesPerUnit = shakesPerUnit
+        self.animatableData = animatableData
+    }
+
+    public func effectValue(size _: CGSize) -> ProjectionTransform {
+        ProjectionTransform(CGAffineTransform(translationX:
+            amount * sin(animatableData * .pi * CGFloat(shakesPerUnit)),
+            y: 0))
+    }
+}
