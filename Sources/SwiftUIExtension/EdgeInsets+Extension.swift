c0c20f9784ae65d0d1845ef047cce30abc345aae
Long Vu <long.vudai01@gmail.com>
2026-01-05 16:39:44 +0700

    Add Hashable and Codable conformance to EdgeInsets extension

--- Sources/SwiftUIExtension/EdgeInsets+Extension.swift
+++ Sources/SwiftUIExtension/EdgeInsets+Extension.swift
@@ -26,3 +26,35 @@ public extension EdgeInsets {
         )
     }
 }
+
+extension EdgeInsets: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(top)
+        hasher.combine(leading)
+        hasher.combine(bottom)
+        hasher.combine(trailing)
+    }
+}
+
+extension EdgeInsets: Codable {
+    enum CodingKeys: String, CodingKey {
+        case top, leading, bottom, trailing
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let top = try container.decode(CGFloat.self, forKey: .top)
+        let leading = try container.decode(CGFloat.self, forKey: .leading)
+        let bottom = try container.decode(CGFloat.self, forKey: .bottom)
+        let trailing = try container.decode(CGFloat.self, forKey: .trailing)
+        self.init(top: top, leading: leading, bottom: bottom, trailing: trailing)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(top, forKey: .top)
+        try container.encode(leading, forKey: .leading)
+        try container.encode(bottom, forKey: .bottom)
+        try container.encode(trailing, forKey: .trailing)
+    }
+}
